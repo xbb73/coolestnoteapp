d752371a77b04593712de59de9528460d43cf8c3
xbb73 <xbb73@outlook.com>
2023-10-23 10:15:01 -0400

    graph

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "coolestnoteapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "coolestnoteapp99ecef59": {
       "AppClientID": "string",
