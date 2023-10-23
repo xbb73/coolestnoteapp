96a3eb52cdab83f4e81dadcc8751f0ee99c6c2b8
xbb73 <xbb73@outlook.com>
2023-10-23 11:12:45 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import { Storage } from "aws-amplify";
 import config from "./aws-exports";
 
 Amplify.configure(config);
